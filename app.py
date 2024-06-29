38b5ba6dd98ef281994f685611fb8e6707283a7b
Alonso <alonsolopezdominguez3@gmail.com>
2024-06-28 21:27:03 -0600

    Update PORT:5000

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+import os
 
 app = Flask(__name__)
 
@@ -30,4 +31,6 @@ def generate():
     return jsonify({'poem': poem})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
+    app.run(host='0.0.0.0', port=port, debug=debug)
